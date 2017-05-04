61c91a9b7b8f293458bbd2ccf424c7927273e897
Frithjof Winkelmann <frithjof97@web.de>
2017-05-04 10:07:17 +0200

    Test gridsearch

--- src/Examples/GridSearch.hs
+++ src/Examples/GridSearch.hs
@@ -24,11 +24,12 @@ drawState (State g (x,y) v _) = Pictures [
 traceR x = traceShow x x
 
 doLrta :: State (Array (Int,Int) Double) -> State (Array (Int,Int) Double)
-doLrta s@(State (Grid g) p (Grid v) i) = case lrta p succsessor (i!) of
+doLrta s@(State (Grid g) p (Grid v) i) = case lrta p succsessor (i!) (const . const $ 2) of
   Just (d,a) -> State (Grid g) a (Grid $ v//[(p,True)]) (i//[(p,traceR d)])
   Nothing -> s
   where
-    succsessor (x,y) = filter (\p -> inBounds (bounds v) p && not (g!p)) [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+    succsessor (x,y) = filter (\p -> inBounds (bounds v) p && not (g!p)) (list x y)
+    list x y = if x > y then [(x,y-1),(x,y+1),(x-1,y),(x+1,y)] else [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
 
 inBounds :: ((Int,Int),(Int,Int)) -> (Int,Int) -> Bool
 inBounds ((x1,y1),(x2,y2)) (x,y) = x>=x1 && y>=y1 && x<=x2 && y<=y2
@@ -43,10 +44,10 @@ initState g = State g (2,2) vis $
 
 test = do
   g <- grid2
-  simulate (InWindow "Yay" (200,200) (200,200)) white 2 (initState $ Grid g) drawState (\_ _ m -> doLrta m)
+  simulate (InWindow "Yay" (400,400) (200,200)) white 20 (initState $ Grid g) drawState (\_ _ m -> doLrta m)
 
-sizeX = 40
-sizeY = 40
+sizeX = 300
+sizeY = 300
 
 grid2 = listArray ((1,1),(sizeX,sizeY)) <$> sequenceA [if x==1||y==1||x==sizeX||y==sizeY then return True
   else (< (0.3 :: Double)) <$> randomIO | x <- [1..sizeX], y <- [1..sizeY]]
--- src/Search/Online.hs
+++ src/Search/Online.hs
@@ -3,10 +3,10 @@ module Search.Online where
 import Data.List (minimumBy)
 import Data.Function (on)
 
-lrta :: a -> (a -> [a]) -> (a -> Double) -> Maybe (Double,a)
-lrta n s h
+lrta :: a -> (a -> [a]) -> (a -> Double) -> (a -> a -> Double) -> Maybe (Double,a)
+lrta n s h st
   | h n == 0  = Just (0,n)
   | otherwise = case filter ((/= -2) . h) $ s n of
     []  -> Nothing
     [x] -> Just (-2,x)
-    xs  -> let m = minimumBy (compare `on` h) xs in Just (h n + 1,m)
+    xs  -> let m = minimumBy (compare `on` h) xs in Just (h n + st n m,m)
