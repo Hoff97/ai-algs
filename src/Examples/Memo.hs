c0994065109d6cd17181c603b71c68d51ceff2b3
Frithjof Winkelmann <frithjof97@web.de>
2017-10-20 09:26:55 +0200

    Changed LTS Version

--- src/Examples/Memo.hs
+++ src/Examples/Memo.hs
@@ -7,9 +7,9 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-fib' :: (Ord a, Num a) => a -> Memo a a a
+{-fib' :: (Ord a, Num a) => a -> Memo a a a
 fib' = memoize' h
   where
     h 0 = return 0
     h 1 = return 1
-    h n = (+) <$> fib' (n-1) <*> fib' (n-2)
+    h n = (+) <$> fib' (n-1) <*> fib' (n-2)-}
